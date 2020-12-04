7b0da95d0c6cf54309a10c5b68aa05878f041a82
ERshivansh281996 <74892755+ERshivansh281996@users.noreply.github.com>
2020-12-04 23:01:28 +0530

    Create bday1.sh

---
+++ bday1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#Write a shell script that checks when logging in, whether it is any of the known user's
+#birthday today. Design and make use of a simple flat-file database "bday.dat" to store user information.
+#-------------------------------------------------------------------------------------------------------------#
+IFILE="$HOME/bday.csv"
+OFILE=bday_out"$$
+MAILID="guru@gmail.com"
+DAT='date '+%d %b
+DAY=${DAT:0:2}
+MON=echo ${DAT:3:3} | awk '{print toupper($0);}'
+while IFS=";-" read name day month year
+do
+day= 'printf "%02d\n" $day
+month= 'echo $month |awk '{print toupper($0);}'
+if [$day -eq $DAY -a $month = $MON]
+then
+echo $name
+fi
+done < IFILE > OFILE
+
+if [ -f $OFILE -a -s $IFILE]
+then
+sed -i '1i The following user celebrate their bday:\n' $OFILE
+mailx -s "bday on: $DAT" $MAILID < $OFILE
+/rm $OFILE
+echo "Bday mail sent"
+else
+echo "No Bday mail sent"
+fi
+
+
+
+
