99850e4050cb9094571852d8b5610e11ae6dfc95
ERshivansh281996 <74892755+ERshivansh281996@users.noreply.github.com>
2020-12-04 23:06:42 +0530

    Update bday1.sh

--- bday1.sh
+++ bday1.sh
@@ -5,20 +5,20 @@
 IFILE="$HOME/bday.csv"
 OFILE=bday_out"$$
 MAILID="guru@gmail.com"
-DAT='date '+%d %b
+DAT='date '+%d %b''
 DAY=${DAT:0:2}
-MON=echo ${DAT:3:3} | awk '{print toupper($0);}'
-while IFS=";-" read name day month year
+MON=echo ${DAT:3:3} | awk '{print toupper($0);}''
+while IFS=",-" read name day month year
 do
 day= 'printf "%02d\n" $day
-month= 'echo $month |awk '{print toupper($0);}'
+month= 'echo $month |awk '{print toupper($0);}''
 if [$day -eq $DAY -a $month = $MON]
 then
 echo $name
 fi
-done < IFILE > OFILE
+done < $IFILE > $OFILE
 
-if [ -f $OFILE -a -s $IFILE]
+if [ -f $OFILE -a -s $OFILE]
 then
 sed -i '1i The following user celebrate their bday:\n' $OFILE
 mailx -s "bday on: $DAT" $MAILID < $OFILE
