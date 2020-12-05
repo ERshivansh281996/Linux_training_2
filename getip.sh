c709b2ec9c25a2873bb319cf2d70d2c4a8436fc1
ERshivansh281996 <74892755+ERshivansh281996@users.noreply.github.com>
2020-12-05 16:10:51 +0530

    Create getip.sh

---
+++ getip.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Write a shell script to extract the IP address given the network
+#interface name as a parameter.
+#Eg.
+#$ ./getip wlan0
+#192.168.2.10
+#$
+#Include validity checking to verify that the user passed a valid interface (it
+#should exist).
+
+nam=$1
+s=$(ifconfig -s | awk '{print $1}'| grep $nam 2> /dev/null)
+if [ "$s" = "$nam" ]
+then
+ifconfig $nam | awk '{print $2}' | awk 'NR == 2 {print}'
+else
+echo "No Interface"
+fi
