e3a37bddff29dbeb50bfc18b204ac007509b38fc
ERshivansh281996 <74892755+ERshivansh281996@users.noreply.github.com>
2020-12-05 16:24:40 +0530

    Update getip.sh

--- getip.sh
+++ getip.sh
@@ -8,11 +8,4 @@
 #Include validity checking to verify that the user passed a valid interface (it
 #should exist).
 #-------------------------------------------------------------------------------------------------------------------#
-nam=$1
-s=$(ifconfig -s | awk '{print $1}'| grep $nam 2> /dev/null)
-if [ "$s" = "$nam" ]
-then
-ifconfig $nam | awk '{print $2}' | awk 'NR == 2 {print}'
-else
-echo "No Interface"
-fi
+hostname -I | awk '{print $1}'
